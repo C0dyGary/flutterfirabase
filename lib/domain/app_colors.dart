a0fa7598236f953547d50edae5bc1395a8521352
C0dyGary <asenvhale@gmail.com>
2025-11-13 16:10:42 -0400

    feet implemetacion de vista de pagos

---
+++ lib/domain/app_colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primary = Color(0xFFB77466);
+  static const Color secondary = Color(0xFFFFE1AF);
+  static const Color tertiary = Color(0xFFE2B59A);
+  static const Color quaternary = Color(0xFF957C62);
+  static const Color accent = Color(0xFFE2B59A);
+  static const Color background = Color(0xFFfff4e6);
+  static const Color surface = Color(0xFFF5F5F5);
+  static const Color error = Color(0xFFB00020);
+}
+
+class AppContessa {
+  static const Color contessa50 = Color(0xFFFBF6F5);
+  static const Color contessa100 = Color(0xFFF6ECEA);
+  static const Color contessa200 = Color(0xFFEFDDD9);
+  static const Color contessa300 = Color(0xFFE3C4BE);
+  static const Color contessa400 = Color(0xFFD1A298);
+  static const Color contessa500 = Color(0xFFB77466);
+  static const Color contessa600 = Color(0xFFA8675A);
+  static const Color contessa700 = Color(0xFF8C5549);
+  static const Color contessa800 = Color(0xFF75483F);
+  static const Color contessa900 = Color(0xFF634039);
+  static const Color contessa950 = Color(0xFF341F1B);
+}
